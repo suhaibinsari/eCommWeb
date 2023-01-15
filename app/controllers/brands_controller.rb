2d599de539b42505408106eef3713d31979cd8a5
suhaibinsari <mstar390@gmail.com>
2023-01-16 00:51:16 +0500

    updated

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,30 +1,52 @@
 class BrandsController < ApplicationController
+  
   def index
     @brands = Brand.all
    end
- 
    
-   def new
-     @Brand = Brand.new
-     @brands = Brand.all
-   end
+  def show
+    @brand = Brand.find(params[:id])
+
+  end
+  
+  def new
+     @brand = Brand.new
+  end
  
-   def create
-     @Brand = Brand.new(brand_params)
-     
-     if @Brand.save
-       respond_to do |format|
-         format.turbo_stream { render 'index'}
-       end
+  def create
+    @brand = Brand.new(brand_params)
+    # @category.user = current_user
+      if @brand.save!
+        redirect_to brands_path
      else
        render :new
      end
-   end
+  end
    
+  def edit
+    @brand = Brand.find(params[:id])
+  end
+
+  def update
+    @brand = Brand.find(params[:id])
+
+    if @brand.update(brand_params)
+      redirect_to @brand
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  
+  def destroy
+    Brand.find(params[:id]).destroy
+    redirect_to brands_path, notice: "Brand deleted successfully"
+  end
+
    private
  
      def brand_params
        params.require(:brand).permit(:name, :body)
      end
-  end
-  
+     
+end
